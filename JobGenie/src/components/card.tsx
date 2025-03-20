0c8f9a6e6f1565e34a3ac9c6ce04407530eafbca
Ozwald-koaltz <hezekielsitepu@mail.ugm.ac.id>
2025-03-20 21:06:41 +0700

    Update : Style

--- JobGenie/src/components/card.tsx
+++ JobGenie/src/components/card.tsx
@@ -13,25 +13,14 @@ const Card: React.FC<CardProps> = ({
 }) => {
   return (
     <section 
-      className="section"
+      className="card-section"
       style={{
-        flex: 'column',
         backgroundColor: backgroundColor,
       }}>
-      <div
-        style={{
-          borderTopLeftRadius: '25px',
-          color: '#FAF9E7',
-          backgroundColor: 'rgba(255, 255, 255, 0.6)',
-          borderBottomLeftRadius: '25px',
-          borderTopRightRadius: '50px',
-          borderBottomRightRadius: '50px',
-          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-        }}
-      >
-        <h2 style={{ color: "black" }}>{title}</h2>
+      <div className="card-title">
+        <h2>{title}</h2>
       </div>
-      <p style={{ fontSize: "0.875rem", lineHeight: "1.625" }}>
+      <p className="card-text">
         {text}
       </p>
     </section>
